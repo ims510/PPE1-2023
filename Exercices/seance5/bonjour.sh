a63585ab44402e045c76cf0bb9c4923d9848bd35
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-18 09:59:48 +0200

    Ajout commande exit

--- Exercices/seance5/bonjour.sh
+++ Exercices/seance5/bonjour.sh
@@ -4,13 +4,14 @@ echo "le premier argument est: ($1)"
 if [ -z  "$1" ]
 then 
 	echo "le premier argument est vide"
-else
-	echo "le premier argument n'est pas vide"
-	if [[ $1 =~ bon(jour|soir) ]]
-		then
-			echo "salut"
-	fi
-	
+	exit
 fi
+
+
+echo "le premier argument n'est pas vide"
+if [[ $1 =~ bon(jour|soir) ]]
+then
+	echo "salut"
+fi	
 
 echo "fin de programme"
