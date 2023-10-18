ec171b342c34a129d2b337996423b83550d24e4e
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-18 09:53:00 +0200

    Ajout de la commande -n

--- Exercices/seance5/bonjour.sh
+++ Exercices/seance5/bonjour.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "le premier argument est: ($1)"
 
-if [[ $1 =~ bon(jour|soir) ]]
-then
-	echo "salut"
+if [ -n  "$1" ]
+then 
+	echo "le premier argument n'est pas vide"
+	if [[ $1 =~ bon(jour|soir) ]]
+		then
+			echo "salut"
+	fi
+else 
+	echo "le premier argument est vide"
 fi
 
 echo "fin de programme"
