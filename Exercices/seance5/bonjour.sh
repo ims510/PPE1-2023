94a1e7f5e8b22aa2de89d23913d8a14758d28134
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-18 09:58:11 +0200

    Changement -n en -z

--- Exercices/seance5/bonjour.sh
+++ Exercices/seance5/bonjour.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "le premier argument est: ($1)"
 
-if [ -n  "$1" ]
+if [ -z  "$1" ]
 then 
+	echo "le premier argument est vide"
+else
 	echo "le premier argument n'est pas vide"
 	if [[ $1 =~ bon(jour|soir) ]]
 		then
 			echo "salut"
 	fi
-else 
-	echo "le premier argument est vide"
+	
 fi
 
 echo "fin de programme"
