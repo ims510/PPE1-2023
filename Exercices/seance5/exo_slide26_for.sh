7ef3bbead362c872f859c69b025c7142b24ea8b0
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-23 21:31:29 +0200

    Ajoute validation de l'argument au premier exercice

--- Exercices/seance5/exo_slide26_for.sh
+++ Exercices/seance5/exo_slide26_for.sh
@@ -6,9 +6,17 @@
 
 FOLDER_PATH=$1
 
-for YEAR in 2016 2017 2018
-do
-	echo -n "Le nombre de répétitions du mot 'Location' en $YEAR est:"
-	cat $FOLDER_PATH/$YEAR/*/* | grep Location | wc -l
-done
+if [[ $FOLDER_PATH =~ [a-z]*/*[a-z]* ]]
+then
+	for YEAR in 2016 2017 2018
+	do
+		echo -n "Le nombre de répétitions du mot 'Location' en $YEAR est:"
+		cat $FOLDER_PATH/$YEAR/*/* | grep Location | wc -l
+	done
+
+else
+	echo "Le chemin vers les fichiers n'est pas valide"
+		exit
+fi
+
 
