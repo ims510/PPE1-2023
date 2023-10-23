27d3e474bb46a5151310a70ff25ce2e4ca79b8de
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-23 17:10:12 +0200

    Supprime les fichiers qui n'etait pas dans le nouveau dossier

--- Exercices/seance5/line.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read LINE
-do
-	echo "la ligne est: $LINE"
-done
-
-echo "on a fini"
--- Exercices/seance5/read_fichier.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read LINE
-do
-	echo "la ligne est: $LINE"
-done < ./80jours.txt
-
-echo "on a fini"
