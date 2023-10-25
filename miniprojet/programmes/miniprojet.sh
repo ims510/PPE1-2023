9b7414c02faae467edbadbea6ce7fd69321568bb
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-25 10:12:34 +0200

    Ajoute la condition d'existence d'un argument

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 FILE_PATH=$1
+
+if [ $# -ne 1 ] # verifier si le script a un argument
+then
+	echo "ce programme demande un argument: le chemin vers le fichier fr.txt" 
+		exit
+fi
+
 while read -r line;
 do
 	echo ${line};
