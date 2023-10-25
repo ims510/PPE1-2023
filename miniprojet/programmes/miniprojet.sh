e8d4eddf3e8956c293ed56e9984a78a842dfeeca
Ioana Madalina Silai <madutza95@gmail.com>
2023-10-25 11:07:36 +0200

    Ajout du code de reponse HTTP

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -12,6 +12,12 @@ N=1
 
 while read -r line;
 do
-	echo $N	${line};
+	#obtenir le code HTTP and store it in a variable 
+	HTTPCODE=$(curl -s -I  ${line}| grep "HTTP" | egrep -o "[[:digit:]]{3}")
+	echo -e "$N\t$HTTPCODE\t${line}";
 	N=$(expr $N + 1)
-done < "$FILE_PATH/fr.txt"
+done < "$FILE_PATH"
+
+
+#curl -s -I URL | grep "HTTP" | egrep -o "[[:digit:]]{3}"
+#-e for tabs between terms in echo, add \t between things
