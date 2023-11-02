fdc70b25aefdfa3c8662ceebcd2f13defd92bcbe
Ioana Madalina Silai <madutza95@gmail.com>
2023-11-02 16:41:33 +0000

    Ecrit le script pour les exos 1 et 2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -5,7 +5,7 @@ FILE_PATH=$1
 if [ $# -ne 1 ] # verifier si le script a un argument
 then
 	echo "ce programme demande un argument: le chemin vers le fichier fr.txt" 
-		exit
+	exit
 fi
 
 N=1
@@ -13,11 +13,11 @@ N=1
 while read -r line;
 do
 	#obtenir le code HTTP and store it in a variable 
-	HTTPCODE=$(curl -s -I  ${line}| grep "HTTP" | egrep -o "[[:digit:]]{3}")
-	echo -e "$N\t$HTTPCODE\t${line}";
+	CURL=$(curl -s -I -L ${line} | tr -d "\r" ) #obtenir les headers
+	HTTPCODE=$(echo "$CURL" | grep "^HTTP" | egrep -o "[[:digit:]]{3}" | tail -n 1)
+	ENCODING=$(echo "$CURL" | grep "^content-type:" | egrep -o "charset=[^;]*" | cut -f 2 -d =)
+	echo -e "$N\t$HTTPCODE\t${line}\t$ENCODING";
 	N=$(expr $N + 1)
 done < "$FILE_PATH"
 
 
-#curl -s -I URL | grep "HTTP" | egrep -o "[[:digit:]]{3}"
-#-e for tabs between terms in echo, add \t between things
