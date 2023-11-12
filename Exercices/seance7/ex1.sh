a23caeef0df60b9ac9c506113d82064a125e6379
Ioana Madalina Silai <madutza95@gmail.com>
2023-11-12 18:40:31 +0100

    Modifie exo1 pour enlever les apostrophes

--- Exercices/seance7/ex1.sh
+++ Exercices/seance7/ex1.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-FILE_PATH=$1
-
 if [ $# -ne 1 ] # verifier si le script a un argument
 then
 	echo "ce programme demande un argument: le chemin vers le fichier fr.txt" 
 	exit
 fi
 
-ggrep -E -o "[[:alpha:]\'-]*" $FILE_PATH | tr "[:upper:]" "[:lower:]" 
+FILE_PATH=$1
+
+ggrep -E -o "[[:alnum:]-]*" $FILE_PATH | tr "[:upper:]" "[:lower:]" 
+
+# Si on voulait prendre laisser les apostrophes et donc ne pas separer des mots comme qu'il / l'avion etc. on pourait utiliser:
+# ggrep -E -o "[[:alnum:]\'’-]*" $FILE_PATH | tr "[:upper:]" "[:lower:]" 
